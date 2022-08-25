22c7edafe576a11fccaa09d4eefac4c4173812af
Yaser Sharifi <yassersharifi74@gmail.com>
2022-08-25 23:16:57 +0430

    create base of dashboard view

---
+++ src/components/Common/TheFooter.vue
@@ -0,0 +1,4 @@
+<template>
+
+	<h1>Footer</h1>
+</template>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TheHeader from "../components/Common/TheHeader.vue";
+import TheFooter from "../components/Common/TheFooter.vue";
+
+</script>
+
+<template>
+	<div>
+		<TheHeader />
+
+		<br />
+		<br />
+		<br />
+
+		<hr>
+
+		<br>
+		<br>
+		<br>
+		<TheFooter />
+	</div>
+</template>
